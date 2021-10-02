63ab442a13584d5a13d8aacd984cec62fb383b04
TylerPelaez <tyler.pelaez.11@gmail.com>
2021-10-02 17:18:00 -0400

    Add tilemap

---
+++ Assets/GenerateColliders.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GenerateColliders : MonoBehaviour
+{
+    public GameObject ColliderToCreate;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var tilemap = GetComponent<Tilemap>();
+
+        var tilemapSize = tilemap.cellBounds;
+
+        Debug.Log(tilemapSize);
+        for (int i = tilemapSize.x; i < tilemapSize.xMax; i++)
+        {
+            for (int j = tilemapSize.y; j < tilemapSize.yMax; j++)
+            {
+                var currentCell = new Vector3Int(i, j, 0);
+                if (!tilemap.HasTile(currentCell))
+                {
+                    continue;
+                }
+
+                var worldPos = tilemap.CellToLocal(currentCell);
+                Instantiate(ColliderToCreate, worldPos, Quaternion.identity);
+            }
+        }
+        
+    }
+}
