d5c31d31c5109db7a79a0427e4a89f713ca1ff7e
TylerPelaez <tyler.pelaez.11@gmail.com>
2021-10-03 01:08:02 -0400

    Fix a couple bugs w/ win block

--- Assets/Scripts/GravityManager.cs
+++ Assets/Scripts/GravityManager.cs
@@ -9,14 +9,15 @@ public class GravityManager : MonoBehaviour
         UP = 0,
         DOWN = 1,
         LEFT = 2,
-        RIGHT = 3
+        RIGHT = 3,
+        DEFAULT = 4,
     }
 
     public const float GRAVITY_STRENGTH = .1f;
 
     public UnityEvent<Directions> OnChangeGravity;
 
-    private Directions direction;
+    private Directions direction = Directions.DEFAULT;
 
     [SerializeField] private int registeredGravityObjectCount;
     [SerializeField] private int doneMovingGravityObjectCount = 0;
@@ -34,6 +35,21 @@ public class GravityManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        var lifecycleManager = FindObjectOfType<LifeCycleManager>();
+        if (lifecycleManager != null)
+        {
+            lifecycleManager.OnRedo.AddListener(ResetDirection);
+            lifecycleManager.OnUndo.AddListener(ResetDirection);
+        }
+    }
+
+    private void ResetDirection()
+    {
+        direction = Directions.DEFAULT;
+    }
+    
     public void ChangeDirection(Directions newDirection)
     {
         if (!AllGravityObjectsDoneMoving() || newDirection == direction)
