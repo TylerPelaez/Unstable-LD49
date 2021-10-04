6b9c8be623146d37160df90a6d590295027848d0
Jhonys <jhonys.oliveira@hotmail.com>
2021-10-03 23:56:30 -0300

    - Bird idle - arrow importing spritesheet

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,6 +7,7 @@ using UnityEngine.Tilemaps;
 public class CameraController : MonoBehaviour
 {
     private const float extentsToSizeRatio = 2.21428f; // Magic!
+    private const float minBound = 6;
     
     private Vector3 startPosition; // I assume we might want it, idk
     
@@ -18,11 +19,17 @@ public class CameraController : MonoBehaviour
         var tilemapBounds = tilemapManager.GetTilemapBounds();
 
         var largerBound = Math.Max(tilemapBounds.extents.x, tilemapBounds.extents.y);
+        var evenBounds = largerBound * 2.0f % 2f == 0;
         
+        if (largerBound * 2.0 < minBound)
+        {
+            largerBound = evenBounds ? minBound / 2.0f : (minBound + 1) / 2.0f;
+        }
+
         GetComponent<Camera>().orthographicSize =
             extentsToSizeRatio * largerBound;
 
-        var offset = largerBound * 2.0f % 2f == 0 ? new Vector3(0f, -1.5f, -10f) : new Vector3(-0.5f, -1.5f, -10f);
+        var offset = evenBounds ? new Vector3(0f, -1.5f, -10f) : new Vector3(-0.5f, -1.5f, -10f);
 
         transform.position = tilemapBounds.center + offset;
     }
--- Assets/PlayerPrefsManager.cs
+++ Assets/PlayerPrefsManager.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerPrefsManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        // TODO: when actually built, do this!
+        //DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private String GetLevelKey()
     {
-        
+        return "high-score-" + SceneManager.GetActiveScene().buildIndex;
+    }
+    
+    public void SetLevelScore(LifeCycleManager.Score score)
+    {
+        var key = GetLevelKey();
+
+        if ((LifeCycleManager.Score) PlayerPrefs.GetInt(key) < score)
+        {
+            PlayerPrefs.SetInt(key, (int) score);
+            PlayerPrefs.Save();
+        }
     }
 }
